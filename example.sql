ff90daa0337d8b7086db38a9278adb54063850b1
John Fawcett <john@erba.tv>
2012-09-02 10:25:05 +0000

    example sql for creating dynamic routing db

---
+++ example.sql
@@ -0,0 +1,19 @@
+; Examples to create database and table for 
+; testing dynamic routing
+
+
+CREATE DATABASE `customer_routing`;
+
+USE `customer_routing`;
+
+
+CREATE TABLE `customer_routing` (
+  `customer_routing_id` int(11) NOT NULL AUTO_INCREMENT,
+  `callerid` varchar(25) NOT NULL,
+  `result` varchar(25) NOT NULL,
+  PRIMARY KEY (`customer_routing_id`),
+  KEY `callerid` (`callerid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+
