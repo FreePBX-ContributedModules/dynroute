1378c33cd5a9eb75801ea0c11679f7f65431ce4c
John Fawcett <john@voipsupport.it>
2022-05-07 18:57:57 +0200

    Version 16.1.0
    Feature: Add backup and restore
    Feature: Add duplicate button
    Bug fix: Don't give syntax errors if lookup return values contain " char (these are removed since it seems Asterisk cannot handle them in a GotoIf)
    Bug fix: Don't give syntax error when no mysql connection can be established (for example due to wrong host, username or password)
    Bug fix: DTMF was not working when source type None was used (due to a typo in the code)
    Bug fix: result variable not set to empty string if lookup returned empty string
    Bug fix: result variable not set to empty string on various mysql errors
    Bug fix: syntax error when no mysql DB name specified
    Updated: labels and Italian translation
    Removed: unmaintained Dutch translation

--- assets/js/dynroute.js
+++ assets/js/dynroute.js
@@ -1,7 +1,19 @@
-// Copyright (c) 2015-2020 John Fawcett
+// Copyright (c) 2015-2022 John Fawcett
 // This is a dervied work licenced under GPL V3 or later
 // The original file was published by Sagoma Technologies in
 // Freepbx IVR module
+
+$("#duplicate").click(function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        $('input[name="action"]').val("save");
+        name = $('input[name="name"]').val();
+        $('input[name="name"]').val(name + "_COPY_");
+        $('input[name="id"]').val("");
+        $("#extdisplay").val("");
+        $("#frm_dynroute").submit();
+});
+
 
 $(document).ready(function(){
 
